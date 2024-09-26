2eec5749eee3311a49ae553ea8b90971d11c30c4
Parag-deb <paragdeb.pdeb@gmail.com>
2024-09-26 09:17:52 +0600

    done

--- 15. 3Sum.cpp
+++ 15. 3Sum.cpp
@@ -1,16 +1,19 @@
+
+// time complexity = O(n log(n)) + O (n*n)
+
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         vector<vector<int>> ans;
         int n = nums.size();
-        sort(nums.begin() , nums.end());
-        for(int i =0 ; i<n ; i++){
+        sort(nums.begin() , nums.end()); // O(n log(n))
+        for(int i =0 ; i<n ; i++){       // O(n)
             if(i>0 && nums[i]== nums[i-1]){
                 continue;
             }
             int j = i+1 ;
             int k = n-1 ;
-            while(j < k){
+            while(j < k){ // near about n time , like (n-1 , n-2 , ..)
                 int sum = nums[i] + nums[j] + nums[k];
                 if(sum < 0){
                     j++;
